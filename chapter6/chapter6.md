57dcc66cdb9db5652b90bee1194c7758e0163bfd
Garden Windows <garden-windows-eng@pivotal.io>
2018-11-20 17:36:58 -0500

    Finish chapter 6

--- chapter6/chapter6.md
+++ chapter6/chapter6.md
@@ -1,2 +1,35 @@
 # Problems
 
+-How do you access the 4th element of an array or slice?
+`arr[3]`
+
+-What is the length of a slice created using: make([]int, 3, 9)?
+3
+
+-Given the array:
+`x := [6]string{"a","b","c","d","e","f"}`
+what would `x[2:5]` give you?
+A slice that looks like `["c","d","e","f"]`
+
+-Write a program that finds the smallest number in this list:
+
+```golang
+x := []int{
+  48,96,86,68,
+  57,82,63,70,
+  37,34,83,27,
+  19,97, 9,17,
+}
+```
+
+```golang
+min := 0
+
+for _, num := range x {
+    if min == 0 || min > num {
+        min = num
+    }
+}
+
+return num
+```
--- chapter6/main.go
+++ chapter6/main.go
@@ -36,4 +36,52 @@ func main() {
 	fmt.Println(slice1, slice2)
 
 	// Maps
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
+
+	fmt.Println(elements)
+
+	name, ok := elements["Ne"]
+	fmt.Println(name, ok)
+
 }
