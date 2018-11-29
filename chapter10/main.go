55632c97e60f259a9c87969b4b0893a292759dd6
Yechiel Kalmenson <contact@yechiel.me>
2018-11-29 17:36:33 -0500

    complete the goroutines protion of chapter 10

--- chapter10/main.go
+++ chapter10/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func f(n int) {
+	for index := 0; index < 10; index++ {
+		fmt.Println(n, ":", index)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func main() {
+	// goroutines
+	for index := 0; index < 10; index++ {
+		go f(index)
+	}
+	var input string
+	fmt.Scanln(&input)
 }
