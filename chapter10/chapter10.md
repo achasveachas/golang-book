d5941dcf153879bcb1d4ee4189836521638d0258
Yechiel Kalmenson <contact@yechiel.me>
2018-12-03 21:32:42 -0500

    finished chapter 10

--- chapter10/chapter10.md
+++ chapter10/chapter10.md
@@ -1,1 +1,19 @@
 # Problems
+
+- How do you specify the direction of a channel type?
+
+`<- channel` for outgoing, `channel <-` for incoming.
+
+- Write your own Sleep function using time.After.
+
+```golang
+func Sleep(timeOut int) {
+    time.After(time.Second * time.Duration(timeOut))
+}
+```
+
+- What is a buffered channel? How would you create one with a capacity of 20?
+
+A channel with a specific capacity.
+
+`make(chan int, 20)`
--- chapter10/main.go
+++ chapter10/main.go
@@ -2,23 +2,70 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"strconv"
 	"time"
 )
 
-func f(n int) {
-	for index := 0; index < 10; index++ {
-		fmt.Println(n, ":", index)
-		amt := time.Duration(rand.Intn(250))
-		time.Sleep(time.Millisecond * amt)
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping" + strconv.Itoa(i)
+	}
+}
+
+func ponger(c chan string) {
+	for i := 5; ; i++ {
+		c <- "pong" + strconv.Itoa(i)
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
 	}
 }
 
 func main() {
-	// goroutines
-	for index := 0; index < 10; index++ {
-		go f(index)
-	}
+	// var c chan string = make(chan string)
+
+	// go pinger(c)
+	// go ponger(c)
+	// go printer(c)
+
+	// var input string
+	// fmt.Scanln(&input)
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			default:
+				fmt.Println("Nada")
+			}
+		}
+	}()
+
 	var input string
 	fmt.Scanln(&input)
 }
