45c247eb6b8f7a923879fdc5b84e4f4c9f9b960d
Yechiel Kalmenson <contact@yechiel.me>
2018-12-13 17:41:42 -0500

    continued chapter 13

--- chapter13/main.go
+++ chapter13/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
-	fmt.Println(
-		// Strings
+	// Strings
 
+	fmt.Println(
 		//Checks if haystack contains needle
 		strings.Contains("test", "es"),
 		// true
@@ -51,6 +55,74 @@ func main() {
 
 		// converts all the letters in a string to upper case
 		strings.ToUpper("aaaaaahhhh"),
-		// "AAAAAAHHHH"
+	// "AAAAAAHHHH"
 	)
+
+	// Files & Folders
+
+	//create a file
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	// remember to close it
+	defer file.Close()
+
+	//write to a file
+	file.WriteString("Hello World")
+
+	// open a file and read it
+	// file, err := os.Open("test.txt")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// // remember to close it
+	// defer file.Close()
+
+	// // get the file size
+	// stat, err := file.Stat()
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// size := stat.Size()
+
+	// read the file
+	// bs := make([]byte, size)
+	// _, err = file.Read(bs)
+
+	//the above can be shortened to:
+	bs, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	str := string(bs)
+	fmt.Println(str)
+
+	// open a directory
+	dir, err := os.Open(".")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//remember to close it
+	defer dir.Close()
+
+	//read the conents of the directory
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
+	}
+
+	//recursively walk through a directory
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	})
+
+	// Errors
+	err = errors.New("New Error!!!!")
+	fmt.Println(err)
 }
